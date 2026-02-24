cdb52749e5827ef262f0b016775e254fe3087b61
Tydxie <144630941+Tydxie@users.noreply.github.com>
2026-02-24 15:00:14 +0100

    reorganising folders, first attempt to separate concerns

---
+++ Thomato Timer/Music/SpotifySecrets.swift
@@ -0,0 +1,12 @@
+//
+//  SpotifySecrets.swift
+//  Thomodoro
+//
+//  Created by Thomas Xie on 2025/11/28.
+//
+
+import Foundation
+
+struct SpotifySecrets {
+    static let clientID = "cd820a8a48f34d7f8ece667dada900f8"
+}
